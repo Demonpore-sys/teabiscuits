45eca5e7372c3999f33afee8ae677df528831ca7
Nava Whiteford <new@sgenomics.org>
2020-07-05 01:36:16 +0900

    Added test code

---
+++ teabiscuits_adc/testcode/testcode.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE0)); 
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+}
+
+void loop() {
+  SPI.begin();
+  // put your main code here, to run repeatedly:
+  long adcData = 0;
+  long spiData = 0;
+  long hi = 0, lo = 0;
+
+  delay(10);
+  spiData = SPI.transfer(0x00);
+  delay(10);
+  digitalWrite(2, HIGH); // do acq
+
+  delay(10);
+  digitalWrite(2, LOW); // do xfer
+  delay(10);
+  
+  spiData = SPI.transfer(0x00);
+  hi = spiData;
+
+  spiData = SPI.transfer(0x00);
+  lo = spiData;
+
+  digitalWrite(2, HIGH); 
+  delay(10);
+  adcData  = hi << 8;
+  adcData += lo;
+  Serial.println(adcData);
+}
